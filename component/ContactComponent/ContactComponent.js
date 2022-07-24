7b9eb49b5256e0163eaa1acfac9a8c3907ee3f54
ahmedismail4494 <ahmed_ismail4494@yahoo.com>
2022-07-24 06:26:29 +0200

    2nd commit

--- component/ContactComponent/ContactComponent.js
+++ component/ContactComponent/ContactComponent.js
@@ -33,50 +33,50 @@ const ContactComponent = () => {
           <div className={Cstyle.info2 + " col-md-6"}>
             {/* Form Section */}
             <form >
-              <div class="form-row m-auto">
-                <div class="form-group col-md-6">
+              <div className="form-row m-auto">
+                <div className="form-group col-md-6">
                   <label for="inputEmail4">Email</label>
-                  <input type="email" class="form-control" id="inputEmail4" placeholder="Email" />
+                  <input type="email" className="form-control" id="inputEmail4" placeholder="Email" />
                 </div>
-                <div class="form-group col-md-6">
+                <div className="form-group col-md-6">
                   <label for="inputPassword4">Password</label>
-                  <input type="password" class="form-control" id="inputPassword4" placeholder="Password" />
+                  <input type="password" className="form-control" id="inputPassword4" placeholder="Password" />
                 </div>
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <label for="inputAddress">Address</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" />
+                <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <label for="inputAddress2">Address 2</label>
-                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
+                <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
               </div>
-              <div class="form-row">
-                <div class="form-group col-md-6">
+              <div className="form-row">
+                <div className="form-group col-md-6">
                   <label for="inputCity">City</label>
-                  <input type="text" class="form-control" id="inputCity" />
+                  <input type="text" className="form-control" id="inputCity" />
                 </div>
-                <div class="form-group col-md-4">
+                <div className="form-group col-md-4">
                   <label for="inputState">State</label>
-                  <select id="inputState" class="form-control">
+                  <select id="inputState" className="form-control">
                     <option selected>Choose...</option>
                     <option>...</option>
                   </select>
                 </div>
-                <div class="form-group col-md-2">
+                <div className="form-group col-md-2">
                   <label for="inputZip">Zip</label>
-                  <input type="text" class="form-control" id="inputZip" />
+                  <input type="text" className="form-control" id="inputZip" />
                 </div>
               </div>
-              <div class="form-group">
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox" id="gridCheck" />
-                  <label class="form-check-label" for="gridCheck">
+              <div className="form-group">
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" id="gridCheck" />
+                  <label className="form-check-label" for="gridCheck">
                     Check me out
                   </label>
                 </div>
               </div>
-              <button type="submit" class="btn btn-primary">Sign in</button>
+              <button type="submit" className="btn btn-primary">Sign in</button>
             </form>
           </div>
         </div>
